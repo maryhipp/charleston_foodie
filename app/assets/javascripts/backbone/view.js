fb7379dc5287c6f4432675e6e5c6962a05581836
maryhipp <maryhipp@gmail.com>
2015-06-22 20:16:35 -0400

    jsonp

--- app/assets/javascripts/backbone/view.js
+++ app/assets/javascripts/backbone/view.js
@@ -9,7 +9,7 @@ var ListView = Backbone.View.extend({
   render: function(restaurant) {
     var html = '';
     this.collection.each(function(restaurant) {
-      html += '<div><a href=' + restaurant.get('url') + ' target=_blank><img src=' + restaurant.get('image_url') + ' class="restaurant-image"></a><div class="restaurant-info"><a href=' + restaurant.get('url') + ' target=_blank>' + restaurant.get('name') + ' </a><br><img src=' + restaurant.get('rating_img_url_small') + '></div></div>';
+      html += '<div class="result"><a href=' + restaurant.get('url') + ' target=_blank><img src=' + restaurant.get('image_url') + ' class="restaurant-image"></a><div class="restaurant-info"><a href=' + restaurant.get('url') + ' target=_blank>' + restaurant.get('name') + ' </a><br><img src=' + restaurant.get('rating_img_url_small') + '></div></div>';
     })
     this.$el.html(html);
   }
