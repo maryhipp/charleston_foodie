cd8eda25ea6d93c6326c215bb2aa9c95123fc8aa
maryhipp <maryhipp@gmail.com>
2015-06-22 20:21:17 -0400

    https

--- app/assets/javascripts/backbone/model.js
+++ app/assets/javascripts/backbone/model.js
@@ -17,19 +17,6 @@ var RestaurantsCollection = Backbone.Collection.extend({
     return 'http://young-island-3325.herokuapp.com/?q=' + this.searchTerm;
   },
 
-  sync : function(method, collection, options) {
-    // By setting the dataType to "jsonp", jQuery creates a function
-    // and adds it as a callback parameter to the request, e.g.:
-    // [url]&callback=jQuery19104472605645155031_1373700330157&q=bananarama
-    // If you want another name for the callback, also specify the
-    // jsonpCallback option.
-    // After this function is called (by the JSONP response), the script tag
-    // is removed and the parse method is called, just as it would be
-    // when AJAX was used.
-    options.dataType = "jsonp";
-    return Backbone.sync(method, collection, options);
-  },
-
   search: function(query) {
     this.searchTerm = query;
     this.fetch();
